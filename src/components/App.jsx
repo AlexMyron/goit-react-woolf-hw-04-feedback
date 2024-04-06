3d1d8015f5354dc6095f9360a43d5fd337dc6892
Olek Myron <myrondell@gmail.com>
2024-04-06 09:43:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,63 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [feedbackState, setFeedbackState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const onLeaveFeedback = name => {
+    setFeedbackState(prev => ({ ...prev, [name]: prev[name] + 1 }));
   };
 
-  onLeaveFeedback = name => {
-    this.setState(prev => {
-      return { ...prev, [name]: prev[name] + 1 };
-    });
-  };
+  const countTotalFeedback = () =>
+    Object.entries(feedbackState).reduce((total, stat) => (total += stat[1]), 0);
 
-  countTotalFeedback = () =>
-    Object.entries(this.state).reduce((total, stat) => (total += stat[1]), 0);
+  const countPositiveFeedbackPercentage = total =>
+    Math.round((feedbackState.good * 100) / total);
 
-  countPositiveFeedbackPercentage = total =>
-    Math.round((this.state.good * 100) / total);
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positive = this.countPositiveFeedbackPercentage(total);
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          padding: '60px',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please, Leave a Feedback">
-          <FeedbackOptions
-            buttonHandler={this.onLeaveFeedback}
-            options={{ good, neutral, bad }}
+  const { good, neutral, bad } = feedbackState;
+  const total = countTotalFeedback();
+  const positive = countPositiveFeedbackPercentage(total);
+  
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '60px',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please, Leave a Feedback">
+        <FeedbackOptions
+          buttonHandler={onLeaveFeedback}
+          options={{ good, neutral, bad }}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positive || 0}
           />
-        </Section>
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positive || 0}
-            />
-          ) : (
-            <Notification message="There is no feedback provided yet." />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback provided yet." />
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export { App };
